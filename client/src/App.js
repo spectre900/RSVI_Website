04ea54f01d7ac6b687d2f51f77621b1228773ea6
spectre900 <nayakpratham0@gmail.com>
2021-06-03 16:38:58 +0530

    Home page update

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,11 @@
 import Header from './Components/Header/Header';
-import Rehabilitation from './Components/Home/Rehabilitation';
-import Mission from './Components/Home/Mission';
-import Journey from './Components/Home/Journey';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div>
       <Header />
-      <Rehabilitation />
-      <Mission />
-      <Journey />
+      <Home />
     </div>
   );
 }
