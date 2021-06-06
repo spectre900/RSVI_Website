1133c226169026402d78ad3415dcba4dcb27d14e
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-06-06 19:17:41 +0530

    Update Vacancy

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
+import Internship from "./Components/Internship/Internship";
+
+// This is the Default part
+// function App() {
+//   return (
+//     <div>
+//       <Header />
+//       <Home />
+//     </div>
+//   );
+// }
 
 function App() {
-  return (
-    <div>
-      <Header />
-      <Home />
-    </div>
-  );
+  return (<div>
+    <Header/>
+    <Internship/>
+  </div>);
 }
 
 export default App;
--- client/src/Components/Internship/Vacancies/FlipCard.js
+++ client/src/Components/Internship/Vacancies/FlipCard.js
@@ -7,14 +7,10 @@ import CardContentBack from "./CardContentBack";
 function FlipCard(params) {
 
   return (<Flippy flipOnHover={true} flipOnClick={true} flipDirection="horizontal" className={Style.card}>
-    <FrontSide animationDuration="300" style={{
-        backgroundColor: '#FF9D96'
-      }}>
+    <FrontSide animationDuration="300">
       <CardContentFront image={params.img} heading={params.head}/>
     </FrontSide>
-    <BackSide animationDuration="300" style={{
-        backgroundColor: '#FF9D96'
-      }}><CardContentBack/></BackSide>
+    <BackSide animationDuration="300"><CardContentBack/></BackSide>
   </Flippy>)
 }
 
