8665d236fce544d2a9541b5375f10628ea06018b
Ayush Dhanraj <dhanrajaayush123@gmail.com>
2021-06-06 19:18:54 +0530

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,18 @@ import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 import Internship from "./Components/Internship/Internship";
 
-// This is the Default part
-// function App() {
-//   return (
-//     <div>
-//       <Header />
-//       <Home />
-//     </div>
-//   );
-// }
-
 function App() {
   return (<div>
     <Header/>
-    <Internship/>
+    <Home/>
   </div>);
 }
+
+// function App() {
+//   return (<div>
+//     <Header/>
+//     <Internship/>
+//   </div>);
+// }
 
 export default App;
