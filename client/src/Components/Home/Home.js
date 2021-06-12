d2c1a2d6815da95df6f82f01371dcc13b4644f1b
Ritul Deshmukh <ritulrdeshmukh@gmail.com>
2021-06-12 20:55:23 +0530

    collaboration and carousel

--- client/src/Components/Home/Home.js
+++ client/src/Components/Home/Home.js
@@ -1,18 +1,23 @@
 import React, { Component } from 'react';
-
+import Collaboration from './Collaboration';
 import Rehabilitation from './Rehabilitation';
 import Mission from './Mission';
 import Journey from './Journey';
 import Success from './Success';
+import {SliderData} from './SliderData';
+import ImageSlider from './ImageSlider';
+
 
 class Home extends Component {
     render() {
         return (
             <div>
+                <ImageSlider slides={SliderData}/>
                 <Rehabilitation />
                 <Mission />
                 <Journey />
                 <Success/>
+                <Collaboration/>
             </div>
         );
     }
---
+++ client/src/Components/Home/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+    {
+        image: "https://drive.google.com/uc?id=1u81uXxWs8N2bEHe-cbQJKu5SM4j6PXNq"
+    },
+    {
+        image: "https://drive.google.com/uc?id=1-YGuc9Ilxfcf5WhwKkloGUtXBjyAsoEA"
+    },
+    {
+        image: "https://drive.google.com/uc?id=1-q4tY2aUJ4Yq0QirvixYzBEOr4JeG8gQ"
+    },
+    {
+        image: "https://drive.google.com/uc?id=18fRfZ8sxYwWUPyKfelbFUrZRSLVzQm4S"
+    },
+    {
+        image: "https://drive.google.com/uc?id=1l0ymCDkabmbV2NdlJPqJuPVKzGBEu8xn"
+    },
+    {
+        image: "https://drive.google.com/uc?id=1b93UXzpTWd4vJo7pw3tCaykZToTLFsPC"
+    },
+    {
+        image: "https://drive.google.com/uc?id=1xgkEhdHOhoGcIzBw4wu5xba8BtvfPzKy"
+    }
+]
