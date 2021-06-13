4e1401b9f2f12c1bb034e2436ecbe45e38c6cf15
Ritul Deshmukh <ritulrdeshmukh@gmail.com>
2021-06-13 08:53:26 +0530

    what we do component

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import Home from './Components/Home/Home';
 import Internship from "./Components/Internship/Internship";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Footer from './Components/Footer/Footer';
+import WhatWeDo  from './Components/Home/WorksComp/WhatWeDo';
 
 
 function App() {
@@ -10,7 +11,10 @@ function App() {
     <Router>
     <div>
       <Header />
-      <Home />
+      <Switch>
+         <Route exact path='/' component={Home} />
+         <Route path='/' component={WhatWeDo}/>
+      </Switch>
       <Footer />
     </div>
     </Router>
