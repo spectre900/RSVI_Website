f186e8af457d91ac99f642c335cbf1f8e7d205f7
sourabh <sourabhkattimani01@gmail.com>
2021-06-16 23:03:11 +0530

    internship page added

--- client/src/Components/Internship/Internship.js
+++ client/src/Components/Internship/Internship.js
@@ -1,8 +1,18 @@
 import React from "react";
 import Vacancy from "./Vacancies/Vacancy";
+import Joinus from "./Joinus/Joinus";
+import Perks from "./Perks/Perks";
+
 
 function Internship() {
-  return (<div><Vacancy/></div>)
+  return (
+    <div>
+      <Joinus />
+      <Vacancy />
+       <Perks/>
+    </div>
+  );
+
 }
 
 export default Internship;
---
+++ client/src/Components/Internship/Joinus/Links.js
@@ -0,0 +1,3 @@
+const joinUsImg = "https://drive.google.com/thumbnail?id=15OTyf_7_L8l6kWW_83exdRbnWQHXIQu-";
+
+export default joinUsImg;
