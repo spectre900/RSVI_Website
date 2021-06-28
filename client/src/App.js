5eaa659c6ad1c5eef41e0a21738b8550376925fc
spectre900 <nayakpratham0@gmail.com>
2021-06-28 19:19:33 +0530

    minor changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,14 @@
-import Header from './Components/Header/Header';
-// import Footer from './Components/Footer/Footer';
-
-import Home from './Components/Home/Home';
-//import Internship from "./Components/Internship/Internship";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import Footer from './Components/Footer/Footer';
+
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 import WhatWeDo  from './Components/Home/WorksComp/WhatWeDo';
-//import AboutUs from './Components/AboutUs/AboutUs';
 import ActivityChild from './Components/Home/Activity/ActivityChild';
+import AboutUs from './Components/AboutUs/AboutUs';
+import GetInTouch from './Components/GetInTouch/GetInTouch';
+import Footer from './Components/Footer/Footer';
+
+//import Internship from "./Components/Internship/Internship";
 
 function App() {
   return (
@@ -18,6 +19,8 @@ function App() {
          <Route exact path='/' component={Home} />
          <Route path='/work/' component={WhatWeDo}/>
          <Route exact path="/ActivityChild" component={ActivityChild}/>
+         <Route exact path='/about-us' component={AboutUs} />
+         <Route exact path='/get-in-touch' component={GetInTouch} />
       </Switch>
       <Footer />
     </div>
