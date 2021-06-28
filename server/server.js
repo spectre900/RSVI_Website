ac60d5cedca22e66abbb9c1f05e36300fce59d2d
Vinayak1298 <vinayakmirani1298@gmail.com>
2021-06-29 00:51:15 +0530

    testing mock

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path')
+const app = express();
+
+app.use(cors());
+// API
+
+
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
