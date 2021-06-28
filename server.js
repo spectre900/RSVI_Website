152373115447a83ea045e9f2a4964fdfa06ea10c
Vinayak1298 <vinayakmirani1298@gmail.com>
2021-06-29 01:23:19 +0530

    t8

--- server.js
+++ server.js
@@ -8,14 +8,10 @@ app.use(cors());
 // API
 
 
-if (process.env.NODE_ENV === 'production') {
-    // Set static folder
-    app.use(express.static('client/build'));
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-    });
-  }
+app.use(express.static(path.join(__dirname, '../build')))
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../build'))
+})
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server started on port ${port}`);
