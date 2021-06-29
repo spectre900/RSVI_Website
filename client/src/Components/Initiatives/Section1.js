d72047b950eaf40fd60faf2ad3881d384e1e404f
ar30612 <ar30612@gmail.com>
2021-06-29 16:26:55 +0200

    Boilerplate_Initiatives

---
+++ client/src/Components/Initiatives/Section1.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Section1 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default Section1
