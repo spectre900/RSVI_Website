b4188e7505ddb06c139d258c837a1607f15bc782
rishita13 <62659650+RISHITA13@users.noreply.github.com>
2021-07-05 22:11:08 +0530

    Faq page

---
+++ client/src/Components/Faqs/Faqs.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import Faqsection from './Faqsection/Faqsection';
+
+class Faqs extends Component {
+    render() {
+        return (
+            <div>
+                <Faqsection />
+            </div>
+        );
+    }
+}
+export default Faqs;
