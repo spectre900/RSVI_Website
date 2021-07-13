f1e14f931335a86598bfa37f10ec4420f08b9530
spectre900 <nayakpratham0@gmail.com>
2021-07-13 23:16:23 +0530

    collaboration form

--- client/src/Components/AboutUs/Collab/FormSection.js
+++ client/src/Components/AboutUs/Collab/FormSection.js
@@ -1,44 +1,32 @@
 import React, { Component } from 'react';
 
-import styles from './FormSection.module.css';
-import { Form } from 'react-bootstrap';
-import {Button} from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Row, Container, Col} from 'react-bootstrap';
 
+import styles from './FormSection.module.css';
 class FormSection extends Component {
   render() {
     return (
-      <>
-      <div>
-      <br/>
-    <p className={styles.tt}>  Fill the form below to collaborate with us: </p>
-      </div>
-      <div className={styles.formsection}>
-      <p className={styles.tt1}>
-          ( * symbolises compulsory questions.)
-      </p>
-<div className={styles.tt3}>
-      <Form.Group>
-
-  <Form.Control className={styles.tt4} size="lg" type="text" placeholder="Name *: "/>
-
-  <Form.Control className={styles.tt4} size="lg" type="text" placeholder="Email Id *: " />
-
-<Form.Control className={styles.tt4} size="lg" type="text" placeholder="Organisation/Individual: " />
-</Form.Group>
-</div>
-
-
-<Form.Group controlId="exampleForm.ControlTextarea1">
-
-   <Form.Control className={styles.tt4} as="textarea" placeholder="How would you like to collaborate with us: *" size="lg" rows={5} columns={6} />
- </Form.Group>
-
-      <br/>
-      <br/>
-      <Button variant="light" type="submit" className={styles.submit}>Submit</Button>
-        </div>
-      </>
-    )
+      <Container fluid className={styles.body}>
+        <p className={styles.header}>
+          Fill the form to collaborate with us:
+        </p>
+        <Row className={styles.holder}>
+          <p className={styles.header}>
+            ( * Compulsory questions.)
+          </p>
+          <form>
+              <input className={styles.input} placeholder='Name *:' required/>
+              <input className={styles.input} placeholder='Email Id *:' required/>
+              <input className={styles.input} placeholder='Organisation/ Individual:'/>
+              <textarea className={styles.textbox} placeholder='How would you like to collaborate with us: *' required/>
+              <button type='submit' className={styles.submit}>
+                Submit
+              </button>
+          </form>
+        </Row>
+      </Container>
+    );
   }
 }
 
