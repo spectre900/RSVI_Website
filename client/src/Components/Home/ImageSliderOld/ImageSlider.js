0a73effa71a3800a822a1929d099bc9221c4ef67
spectre900 <nayakpratham0@gmail.com>
2021-07-16 00:12:33 +0530

    old slider shifted

---
+++ client/src/Components/Home/ImageSliderOld/ImageSlider.js
@@ -0,0 +1,47 @@
+import React,{useState} from 'react';
+import {SliderData} from './SliderData';
+import '../../Assets/Global-Styles/bootstrap.min.module.css';
+// import {FaArrowAltCircleRight} from "react-icons/fa";
+import style from './ImageSlider.module.css';
+import {BsChevronDoubleLeft,BsChevronDoubleRight} from "react-icons/bs";
+
+const ImageSlider = ({slides}) => {
+  
+    const [current, setCurrent] = useState(0);
+    const length = slides.length;
+
+    const nextSlide = () => {
+        setCurrent(current === length - 1 ? 0 : current + 1);
+    };
+    
+    const prevSlide = () => {
+        setCurrent(current === 0 ? length - 1 : current - 1);
+    };
+    
+    if (!Array.isArray(slides) || slides.length <= 0) {
+        return null;
+    }
+
+    return (
+        <div className = {style.is}>
+        <section className={style.slider}>
+      <BsChevronDoubleLeft className={style.leftarrow} onClick={prevSlide} />
+      <BsChevronDoubleRight className={style.rightarrow} onClick={nextSlide} />
+      {SliderData.map((slide, index) => {
+        return (
+          <div
+            className={style.index === current ? 'slide active' : 'slide'}
+            key={index}
+          >
+            {index === current && (
+              <img src={slide.image} alt='travel image' className={style.image} />
+            )}
+          </div>
+        );
+      })}
+    </section>
+    </div>
+    );
+}
+
+export default ImageSlider
---
+++ client/src/Components/Home/ImageSliderOld/SliderData.js
@@ -0,0 +1,23 @@
+export const SliderData = [
+  {
+    image: "https://drive.google.com/uc?id=1LmpgOmv3bPwoRj1q39Tf5yg2_P0Wzz-X",
+  },
+  {
+    image: "https://drive.google.com/uc?id=1TJ-WBYCG28ocV32NF_xUjvPjIlOUjj3C",
+  },
+  {
+    image: "https://drive.google.com/uc?id=1eB1hO6XzNRIDkbdJe6GtGbfMeLB0owgk",
+  },
+  {
+    image: "https://drive.google.com/uc?id=1broS6UnkobcMjewuuB5QGp2gB6O90Cga",
+  },
+  {
+    image: "https://drive.google.com/uc?id=1TwEq-ZryPrrPn-rEqxIB0G__IrGhyMWe",
+  },
+  {
+    image: "https://drive.google.com/uc?id=14GLg39kHbRuki70RMDx4O_IJC5xqM4cr",
+  },
+  {
+    image: "https://drive.google.com/uc?id=1XTDopW7H6kkKfSUeX-LxWUN1OsLPDcZo",
+  },
+];
